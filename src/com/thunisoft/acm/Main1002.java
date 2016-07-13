07e0552488325e969a2eef4fcb0bdc6238a14dc1
wangchuny <wangchuny@wangchuny-PC.thunisoft.com>
2016-07-13 17:29:05 +0800

    新增1002

--- src/com/thunisoft/acm/Main1002.java
+++ src/com/thunisoft/acm/Main1002.java
@@ -1,19 +1,32 @@
 package com.thunisoft.acm;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main1002 {
-	private static Scanner in;
-	public static void main(String[] args) {
-		in = new Scanner(System.in);
-		while (in.hasNextInt()) {
-			int sum = 0;
-			int input = in.nextInt();
-			for(int i =1;i<=input;i++)
-				sum = sum + i;
-			//sum = (1+input)*input/2;
-			System.out.println(sum);
-			System.out.println();
+	private static Scanner scanner;
+
+	public static void main(String args[]) {
+		scanner = new Scanner(System.in);
+		String temp1 = null;
+		String temp2 = null;
+		String result = null;
+		int i;
+		int a = scanner.nextInt();
+		for (i = 0; i < a; i++) {
+			temp1 = scanner.next();
+			temp2 = scanner.next();
+			BigDecimal bigdecimal = new BigDecimal(temp1);
+			BigDecimal bigdecimal2 = new BigDecimal(temp2);
+			result = bigdecimal.add(bigdecimal2).toString();
+			if (i != (a - 1)) {
+				System.out.println("Case" + " " + (i + 1) + ":");
+				System.out.println(bigdecimal + " + " + bigdecimal2 + " = " + result);
+				System.out.println();
+			} else {
+				System.out.println("Case" + " " + (i + 1) + ":");
+				System.out.println(bigdecimal + " + " + bigdecimal2 + " = " + result);
+			}
 		}
 	}
 }
